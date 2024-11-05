1035720e8f22de5e04d1a57c244eacbea2419fd3
Bruno Ribeiro <brunoribeiro.cgms@gmail.com>
2024-11-04 21:04:40 -0400

    editando o arquivo radme e scripts

--- readme.md
+++ readme.md
@@ -5,3 +5,18 @@ id
 nome
 ano
 descrição
+
+- Instruções para inserir ao menos 20 registros
+- Instrução para retornar todos os registro
+- Instrução para retornar um unico registro por id
+- Instrução para retornar apenas nome e ano de todos
+- Instrução para atualizar um registro por id
+- Instrução para excliuir um registro por id
+- criar a tabela
+Usuario
+    id
+    nome
+
+Favorito
+    usuario id
+    filme id
--- scripts.sql
+++ scripts.sql
@@ -13,6 +13,32 @@ create table filme (
 	-- nem todo db vai ter esse tipo
 	descricao text
 );
-
+-- exibindo toda a tabela
 select  * from filme;
+
+-- conta a quantidade den registros
 select  count(*) from filme;
+
+-- Instrução para retornar um unico registro por id
+select * from filme where id = 5;
+
+-- Instrução para retornar apenas nome e ano de todos
+select titulo, ano from filme;
+
+-- Instrução para atualizar um registro por id/ 
+update filme set titulo = "Maquina Mortifera" where id = 5;
+
+-- Instrução para excliuir um registro por id
+delete from filme WHERE id = 5;
+
+-- Criando a tabela Usuario
+create table usuario (
+	id int auto_increment primary key,
+	nome varchar(100) not null
+);
+
+-- Criando a tabela favorito
+create table favorito (
+	usuario_id int,
+	filme_id int
+);
